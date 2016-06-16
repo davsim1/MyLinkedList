d670b300db0ce0c49434e2563577b52ebeb3b752
Simmons <simmonsdm@leidos.com>
2016-06-16 14:19:37 -0400

    Changes including doMethod(num)

--- MyLinkedList/src/list/MyLinkedList.java
+++ MyLinkedList/src/list/MyLinkedList.java
@@ -3,6 +3,7 @@ package list;
 import java.io.Serializable;
 import java.util.AbstractSequentialList;
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
@@ -25,12 +26,6 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 		public Node(E data) {
 			this.data = data;
 		}
-		
-		public Node(Node<E> prev, E data, Node<E> next) {
-			this.prev = prev;
-			this.data = data;
-			this.next = next;
-		}
 
 	}
 
@@ -43,6 +38,13 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 		this.addAll(c);
 	}
 
+	public boolean addAll(int index, Collection<? extends E> c) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return super.addAll(index, c);
+	}
+
 	private void link(Node<E> first, Node<E> second) {
 		if (first != null) {
 			first.next = second;
@@ -51,12 +53,12 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 			second.prev = first;
 		}
 	}
-	
+
 	private void insertNode(Node<E> first, Node<E> newNode, Node<E> last) {
 		link(first, newNode);
 		link(newNode, last);
 	}
-	
+
 	public boolean add(E e) {
 		add(size, e);
 		return true;
@@ -66,11 +68,14 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 	public ListIterator<E> listIterator(int i) {
 		return new ListIterator<E>() {
 			Node<E> prev = null;
-			Node<E> curr = new Node<E>(head, null, head);
+			Node<E> curr = head;
+			Node<E> next = head;
 			boolean canAlter = false;
+			boolean firstNext = true;
+			boolean firstPrev = true;
 			int index = 0;
 			int expectedModCount = modCount;
-			
+
 			@Override
 			public void add(E e) {
 				canAlter = false;
@@ -78,13 +83,18 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 				if (head == null) {
 					head = newNode;
 					tail = head;
-					curr = null;
 				} else {
-					if (curr == null) {
+					if (index == 0) {
+						link(newNode, head);
+						head = newNode;
+					} else if (curr == tail) {
 						link(tail, newNode);
 						tail = newNode;
 					} else {
-						insertNode(curr, newNode, curr.next);
+						if (next == null) {
+							throw new IndexOutOfBoundsException();
+						}
+						insertNode(next.prev, newNode, next);
 					}
 					index++;
 				}
@@ -95,12 +105,12 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 
 			@Override
 			public boolean hasNext() {
-				return curr.next != null;
+				return (curr != null && curr.next != null) || (curr == head && curr != null && firstNext);
 			}
 
 			@Override
 			public boolean hasPrevious() {
-				return curr.prev != null;
+				return (curr != null && curr.prev != null) || (curr == tail && curr != null && firstPrev);
 			}
 
 			@Override
@@ -109,9 +119,21 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 				if (!hasNext()) {
 					throw new NoSuchElementException();
 				}
-				result = curr.next.data;
-				prev = curr;
-				curr = curr.next;
+				if (expectedModCount != modCount) {
+					throw new ConcurrentModificationException();
+				}
+				if (curr == head && curr != null && firstNext) {
+					result = curr.data;
+					prev = curr;
+					firstNext = false;
+					firstPrev = true;
+				} else {
+					result = curr.next.data;
+					prev = curr;
+					curr = curr.next;
+					firstNext = true;
+				}
+				next = curr.next;
 				index++;
 				canAlter = true;
 				return result;
@@ -128,10 +150,21 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 				if (!hasPrevious()) {
 					throw new NoSuchElementException();
 				}
-				
-				result = curr.prev.data;
-				prev = curr;
-				curr = curr.prev;
+				if (expectedModCount != modCount) {
+					throw new ConcurrentModificationException();
+				}
+				if (curr == tail && curr != null && firstPrev) {
+					result = curr.data;
+					prev = curr;
+					firstPrev = false;
+					firstNext = true;
+				} else {
+					result = curr.prev.data;
+					prev = curr;
+					curr = curr.prev;
+					firstPrev = true;
+				}
+				next = curr.next;
 				index--;
 				canAlter = true;
 				return result;
@@ -139,7 +172,7 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 
 			@Override
 			public int previousIndex() {
-				return index;
+				return index - 1;
 			}
 
 			@Override
@@ -148,16 +181,16 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 					throw new IllegalStateException();
 				}
 				canAlter = false;
-				
-				if (prev == tail) {
-					tail = prev.prev; 
+
+				if (curr == tail) {
+					tail = curr.prev;
 				}
-				if (prev == head) {
-					head = prev.next;
+				if (curr == head) {
+					head = curr.next;
 				}
-				
-				link(prev.prev, prev.next);
-				
+
+				link(curr.prev, curr.next);
+
 				size--;
 				index--;
 				expectedModCount++;
@@ -169,16 +202,19 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 				if (!canAlter) {
 					throw new IllegalStateException();
 				}
-				prev.data = e;
+				curr.data = e;
 			}
-			
+
 			private ListIterator<E> goToIndex(int i) {
-				while(i != 0 && hasNext() && nextIndex() <= i) {
+				if (i < 0 || i > size) {
+					throw new IndexOutOfBoundsException();
+				}
+				while (i != 0 && hasNext() && nextIndex() < i) {
 					next();
 				}
 				return this;
 			}
-			
+
 		}.goToIndex(i);
 	}
 
@@ -201,8 +237,8 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 	@Override
 	public Iterator<E> descendingIterator() {
 		return new Iterator<E>() {
-			ListIterator<E> iter = listIterator();
-			
+			ListIterator<E> iter = listIterator(size);
+
 			@Override
 			public boolean hasNext() {
 				return iter.hasPrevious();
@@ -212,7 +248,7 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 			public E next() {
 				return iter.previous();
 			}
-			
+
 			@Override
 			public void remove() {
 				iter.remove();
@@ -260,7 +296,7 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 
 	@Override
 	public E peek() {
-		return head == null? null : head.data;
+		return head == null ? null : head.data;
 	}
 
 	@Override
@@ -270,11 +306,13 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 
 	@Override
 	public E peekLast() {
-		return tail == null? null : tail.data;
+		return tail == null ? null : tail.data;
 	}
 
 	@Override
 	public E poll() {
+		if (size == 0)
+			return null;
 		E result = peek();
 		if (result != null) {
 			remove(0);
@@ -291,7 +329,7 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 	public E pollLast() {
 		E result = peekLast();
 		if (result != null) {
-			remove(size);
+			remove(size - 1);
 		}
 		return result;
 	}
@@ -315,17 +353,17 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 	public E removeFirst() {
 		if (head == null) {
 			throw new NoSuchElementException();
-		} 
+		}
 		return poll();
 	}
 
 	@Override
 	public boolean removeFirstOccurrence(Object o) {
 		ListIterator<E> iter = listIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			if (iter.next().equals(o)) {
 				iter.remove();
-				return true;				
+				return true;
 			}
 		}
 		return false;
@@ -342,13 +380,22 @@ public class MyLinkedList<E> extends AbstractSequentialList<E>
 	@Override
 	public boolean removeLastOccurrence(Object o) {
 		Iterator<E> iter = descendingIterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			if (iter.next().equals(o)) {
 				iter.remove();
-				return true;				
+				return true;
 			}
 		}
 		return false;
+	}
+
+	@Override
+	public Object clone() {
+		MyLinkedList<E> result = new MyLinkedList<E>();
+		for (E e : this) {
+			result.add(e);
+		}
+		return result;
 	}
 
 }
--- MyLinkedList/src/list/Tester.java
+++ MyLinkedList/src/list/Tester.java
@@ -1,7 +1,11 @@
 package list;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
@@ -12,9 +16,31 @@ public class Tester {
 
 	public static void main(String[] args) throws Exception {
 		rand.setSeed(System.currentTimeMillis());
+		LinkedList<Integer> trace = new LinkedList<Integer>();
+		int randTest;
 		test1();
-		populate();
-		doMethod(2);
+		test2();
+		test3();
+		for (int i = 0; i < 100; i++) {
+			populate();
+			for (int j = 1; j <= 39; j++) {
+				doMethod(j);
+			}
+			populate();
+			for (int j = 0; j < 1000; j++) {
+				randTest = rand.nextInt(38) + 1;
+				trace.push(randTest);
+				try {
+					doMethod(randTest);
+				} catch (Exception e) {
+					System.out.println("Test sequence" + trace);
+					System.out.println("   My list: " + myList);
+					System.out.println("Other list: " + list);
+					throw e;
+				}
+			}
+		}
+
 		System.out.println("TESTS PASSED");
 	}
 
@@ -24,9 +50,68 @@ public class Tester {
 		}
 	}
 
+	public static void assertSame(Collection<?> a, Collection<?> b) throws Exception {
+		try {
+			assertTrue(a.toString().equals(b.toString()));
+			assertTrue(a.equals(b));
+			assertTrue(b.equals(a));
+		} catch (Exception e) {
+			System.out.println("   My list: " + myList);
+			System.out.println("Other list: " + list);
+			throw e;
+		}
+	}
+
+	public static void test1() throws Exception {
+		myList.add(1);
+		list.add(1);
+		myList.add(2);
+		list.add(2);
+		myList.add(3);
+		list.add(3);
+		assertSame(myList, list);
+		myList.clear();
+		list.clear();
+	}
+
+	public static void test2() throws Exception {
+		myList.add(1);
+		list.add(1);
+		ListIterator<Integer> e1 = myList.listIterator(list.size());
+		ListIterator<Integer> e2 = list.listIterator(list.size());
+		ArrayList<Integer> l1 = new ArrayList<Integer>();
+		ArrayList<Integer> l2 = new ArrayList<Integer>();
+		while (e1.hasPrevious())
+			l1.add(e1.previous());
+		while (e2.hasPrevious())
+			l2.add(e2.previous());
+
+		assertTrue(l1.equals(l2));
+
+	}
+
+	public static void test3() throws Exception {
+		LinkedList<Character> charList = new LinkedList<Character>();
+		MyLinkedList<Character> myCharList = new MyLinkedList<Character>();
+		charList.add('A');
+		myCharList.add('A');
+		ListIterator<Character> iter = charList.listIterator();
+		ListIterator<Character> myIter = myCharList.listIterator();
+
+		for (int i = 0; i < 10; i++) {
+			assertTrue(iter.next().equals('A'));
+			assertTrue(iter.previous().equals('A'));
+			assertTrue(myIter.next().equals('A'));
+			assertTrue(myIter.previous().equals('A'));
+		}
+	}
+
 	public static void populate() throws Exception {
 		int size = rand.nextInt(10);
 		ArrayList<Integer> all = new ArrayList<Integer>();
+
+		myList.clear();
+		list.clear();
 
 		for (int i = 0; i < size; i++) {
 			int num = rand.nextInt(10);
@@ -42,46 +127,439 @@ public class Tester {
 
 	}
 
-	public static void test1() throws Exception {
-		myList.add(1);
-		list.add(1);
-		myList.add(2);
-		list.add(2);
-		myList.add(3);
-		list.add(3);
-
-		assertTrue(myList.toString().equals(list.toString()));
-	}
-
 	public static void doMethod(int num) throws Exception {
 		Integer m = Integer.MIN_VALUE;
+		boolean flag = true;
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		int idx;
+		Integer t;
+		temp.add(11);
+		temp.add(12);
+		temp.add(13);
+
 		switch (num) {
 		case 1:
 			try {
 				m = myList.getFirst();
 			} catch (NoSuchElementException e) {
 				try {
+					flag = false;
 					list.getFirst();
 					assertTrue(false);
 				} catch (NoSuchElementException e2) {
 					// test passes
 				}
 			}
-			assertTrue(m.equals(list.getFirst()));
+			if (flag) {
+				assertTrue(m.equals(list.getFirst()));
+			}
+			assertSame(myList, list);
+			flag = true;
 			break;
 		case 2:
 			try {
 				m = myList.getLast();
 			} catch (NoSuchElementException e) {
 				try {
+					flag = false;
 					list.getLast();
 					assertTrue(false);
 				} catch (NoSuchElementException e2) {
 					// test passes
 				}
 			}
-			assertTrue(m.equals(list.getLast()));
+			if (flag) {
+				assertTrue(m.equals(list.getLast()));
+			}
+			assertSame(myList, list);
+			flag = true;
 			break;
+		case 3:
+			try {
+				m = myList.removeFirst();
+			} catch (NoSuchElementException e) {
+				try {
+					flag = false;
+					list.removeFirst();
+					assertTrue(false);
+				} catch (NoSuchElementException e2) {
+					// test passes
+				}
+			}
+			if (flag) {
+				assertTrue(m.equals(list.removeFirst()));
+			}
+			assertSame(myList, list);
+			flag = true;
+			break;
+		case 4:
+			try {
+				m = myList.removeLast();
+			} catch (NoSuchElementException e) {
+				try {
+					flag = false;
+					list.removeLast();
+					assertTrue(false);
+				} catch (NoSuchElementException e2) {
+					// test passes
+				}
+			}
+			if (flag) {
+				assertTrue(m.equals(list.removeLast()));
+			}
+			assertSame(myList, list);
+			flag = true;
+			break;
+		case 5:
+			myList.addFirst(20);
+			list.addFirst(20);
+			assertSame(myList, list);
+			break;
+		case 6:
+			myList.addLast(25);
+			list.addLast(25);
+			assertSame(myList, list);
+			break;
+		case 7:
+			int n = rand.nextInt(10);
+			assertTrue(myList.contains(n) == list.contains(n));
+			break;
+		case 8:
+			assertTrue(myList.size() == list.size());
+			break;
+		case 9:
+			assertTrue(myList.add(30) == list.add(30));
+			assertSame(myList, list);
+			break;
+		case 10:
+			Integer x = rand.nextInt(10);
+			assertTrue(myList.remove(x) == list.remove(x));
+			assertSame(myList, list);
+			break;
+		case 11:
+			assertTrue(myList.addAll(temp) == list.addAll(temp));
+			assertSame(myList, list);
+			try {
+				myList.addAll(null);
+				assertTrue(false);
+			} catch (NullPointerException e) {
+				// pass
+			}
+			break;
+		case 12:
+			idx = list.size() == 0 ? 0 : rand.nextInt(list.size());
+			temp.clear();
+			temp.add(15);
+			temp.add(16);
+			temp.add(17);
+			assertTrue(myList.addAll(idx, temp) == list.addAll(idx, temp));
+			assertSame(myList, list);
+			try {
+				myList.addAll(myList.size() + 1, temp);
+				assertTrue(false);
+			} catch (IndexOutOfBoundsException e) {
+				// pass
+			}
+			try {
+				list.addAll(list.size() + 1, temp);
+				assertTrue(false);
+			} catch (IndexOutOfBoundsException e) {
+				// pass
+			}
+			try {
+				myList.addAll(null);
+				assertTrue(false);
+			} catch (NullPointerException e) {
+				// pass
+			}
+			break;
+		case 13:
+			myList.clear();
+			assertTrue(myList.size() == 0);
+			assertTrue(myList.toString() == "[]");
+			assertTrue(myList.equals(new ArrayList<Integer>()));
+			myList.addAll(list);
+			assertSame(myList, list);
+			break;
+		case 14:
+			if (list.size() > 0) {
+				idx = rand.nextInt(list.size());
+				assertTrue(myList.get(idx).equals(list.get(idx)));
+			}
+			try {
+				myList.get(-1);
+				assertTrue(false);
+			} catch (IndexOutOfBoundsException e) {
+				// pass
+			}
+			try {
+				myList.get(list.size() + 1);
+				assertTrue(false);
+			} catch (IndexOutOfBoundsException e) {
+				// pass
+			}
+			break;
+		case 15:
+			if (list.size() == 0) {
+				try {
+					myList.set(0, 999);
+					assertTrue(false);
+				} catch (IndexOutOfBoundsException e) {
+					// pass
+				}
+			} else {
+				idx = rand.nextInt(list.size());
+				assertTrue(myList.set(idx, myList.get(idx) + 1).equals(list.set(idx, list.get(idx) + 1)));
+				assertSame(myList, list);
+			}
+			break;
+		case 16:
+			idx = list.size() == 0 ? 0 : rand.nextInt(list.size());
+			myList.add(idx, 50);
+			list.add(idx, 50);
+			assertSame(myList, list);
+			break;
+		case 17:
+			if (list.size() == 0) {
+				try {
+					myList.remove(0);
+					assertTrue(false);
+				} catch (IndexOutOfBoundsException e) {
+					// pass
+				}
+			} else {
+				idx = rand.nextInt(list.size());
+				assertTrue(myList.remove(idx).equals(list.remove(idx)));
+				assertSame(myList, list);
+			}
+			break;
+		case 18:
+			m = rand.nextInt(10);
+			assertTrue(myList.indexOf((Integer) m) == list.indexOf((Integer) m));
+			break;
+		case 19:
+			m = rand.nextInt(
+					10);/*
+						 * System.out.println(myList); System.out.println(list);
+						 * System.out.println("m " + m);
+						 * System.out.println(myList.lastIndexOf((Integer)m));
+						 * System.out.println(list.lastIndexOf((Integer)m));
+						 */
+			assertTrue(myList.lastIndexOf((Integer) m) == list.lastIndexOf((Integer) m));
+			break;
+		case 20:
+			if (myList.peek() == null) {
+				assertTrue(list.peek() == null);
+			} else {
+				assertTrue(myList.peek().equals(list.peek()));
+			}
+			break;
+		case 21:
+			if (myList.isEmpty()) {
+				try {
+					myList.element();
+					assertTrue(false);
+				} catch (NoSuchElementException e) {
+					// pass
+				}
+			} else {
+				assertTrue(myList.element().equals(list.element()));
+			}
+			break;
+		case 22:
+			if ((t = myList.poll()) == null) {
+				assertTrue(list.poll() == null);
+			} else {
+				assertTrue(t.equals(list.poll()));
+			}
+			assertSame(myList, list);
+			break;
+		case 23:
+			if (list.size() == 0) {
+				try {
+					myList.remove();
+					assertTrue(false);
+				} catch (NoSuchElementException e) {
+					// pass
+				}
+			} else {
+				assertTrue(myList.remove().equals(list.remove()));
+			}
+			assertSame(myList, list);
+			break;
+		case 24:
+			assertTrue(myList.offer(40) == (list.offer(40)));
+			assertSame(myList, list);
+			break;
+		case 25:
+			assertTrue(myList.offerFirst(41) == (list.offerFirst(41)));
+			assertSame(myList, list);
+			break;
+		case 26:
+			assertTrue(myList.offerLast(42) == (list.offerLast(42)));
+			assertSame(myList, list);
+			break;
+		case 27:
+			assertTrue(myList.peekFirst() == list.peekFirst() || myList.peekFirst().equals(list.peekFirst()));
+			break;
+		case 28:
+			assertTrue(myList.peekLast() == list.peekLast() || myList.peekLast().equals(list.peekLast()));
+			break;
+		case 29:
+			if ((t = myList.pollFirst()) == null) {
+				assertTrue(list.pollFirst() == null);
+			} else {
+				assertTrue(t.equals(list.pollFirst()));
+			}
+			assertSame(myList, list);
+			break;
+		case 30:
+			if ((t = myList.pollLast()) == null) {
+				assertTrue(list.pollLast() == null);
+			} else {
+				assertTrue(t.equals(list.pollLast()));
+			}
+			assertSame(myList, list);
+			break;
+		case 31:
+			m = rand.nextInt(10);
+			myList.push(m);
+			list.push(m);
+			assertSame(myList, list);
+			break;
+		case 32:
+			try {
+				m = myList.pop();
+			} catch (NoSuchElementException e) {
+				try {
+					flag = false;
+					list.pop();
+					assertTrue(false);
+				} catch (NoSuchElementException e2) {
+					// test passes
+				}
+			}
+			if (flag) {
+				assertTrue(m.equals(list.pop()));
+			}
+			flag = true;
+			assertSame(myList, list);
+			break;
+		case 33:
+			m = rand.nextInt(10);
+			assertTrue(myList.removeFirstOccurrence((Integer) m) == (list.removeFirstOccurrence((Integer) m)));
+			assertSame(myList, list);
+			break;
+		case 34:
+			m = rand.nextInt(10);
+			assertTrue(myList.removeLastOccurrence((Integer) m) == (list.removeLastOccurrence((Integer) m)));
+			assertSame(myList, list);
+			break;
+		case 35:
+			// Verify forwards and backwards iteration
+			ListIterator<Integer> e1 = myList.listIterator(list.size());
+			ListIterator<Integer> e2 = list.listIterator(list.size());
+			ArrayList<Integer> l1 = new ArrayList<Integer>();
+			ArrayList<Integer> l2 = new ArrayList<Integer>();
+			while (e1.hasPrevious())
+				l1.add(e1.previous());
+			while (e2.hasPrevious())
+				l2.add(e2.previous());
+			if (!l1.equals(l2)) {
+				System.out.println("my asc list:    " + l1);
+				System.out.println("other asc list: " + l2);
+			}
+			assertTrue(l1.equals(l2));
+
+			while (e1.hasNext())
+				l1.add(e1.next());
+			while (e2.hasNext())
+				l2.add(e2.next());
+			if (!l1.equals(l2)) {
+				System.out.println("my mix list:    " + l1);
+				System.out.println("other mix list: " + l2);
+			}
+			assertTrue(l1.equals(l2));
+			assertSame(myList, list);
+			// Verify errors
+			if (myList.size() > 2) {
+				try {
+
+					for (Integer i : myList) {
+						myList.remove(myList.peek());
+					}
+
+					assertTrue(false);
+				} catch (ConcurrentModificationException c) {
+					// pass
+				}
+				try {
+
+					for (Integer i : myList) {
+						myList.push(myList.getLast());
+					}
+					assertTrue(false);
+				} catch (ConcurrentModificationException c) {
+					// pass
+				}
+			}
+			Iterator<Integer> e3 = myList.iterator();
+			try {
+				int k = 0;
+				while (k <= myList.size() + 1) {
+					e3.next();
+					k++;
+				}
+				assertTrue(false);
+			} catch (NoSuchElementException e) {
+				// pass
+			}
+
+			// reset
+			myList.clear();
+			myList.addAll(list);
+			break;
+		case 36:
+			Iterator<Integer> desc1 = myList.descendingIterator();
+			Iterator<Integer> desc2 = list.descendingIterator();
+			ArrayList<Integer> lst1 = new ArrayList<Integer>();
+			ArrayList<Integer> lst2 = new ArrayList<Integer>();
+			while (desc1.hasNext())
+				lst1.add(desc1.next());
+			while (desc2.hasNext())
+				lst2.add(desc2.next());
+			if (!lst1.equals(lst2)) {
+				System.out.println("my tail " + myList.getLast());
+				System.out.println("my desc list:    " + lst1);
+				System.out.println("other desc list: " + lst2);
+			}
+			assertTrue(lst1.equals(lst2));
+			break;
+		case 37:
+			MyLinkedList<Integer> myClone = (MyLinkedList<Integer>) myList.clone();
+			LinkedList<Integer> clone = (LinkedList<Integer>) list.clone();
+			assertTrue(myClone.equals(clone));
+			assertTrue(clone != list);
+			assertTrue(myClone != myList);
+			break;
+		case 38:
+			Object[] myArr = myList.toArray();
+			Object[] arr = list.toArray();
+			assertTrue(myArr.length == arr.length);
+			for (int i = 0; i < arr.length; i++) {
+				assertTrue(myArr[i].equals(arr[i]));
+			}
+			break;
+		case 39:
+			Integer[] myIntArr = myList.toArray(new Integer[0]);
+			Integer[] intarr = list.toArray(new Integer[0]);
+			assertTrue(myIntArr.length == intarr.length);
+			for (int i = 0; i < intarr.length; i++) {
+				assertTrue(myIntArr[i].equals(intarr[i]));
+			}
+			break;
+		default:
+			throw new Exception("unknown method");
 		}
 	}
 }
